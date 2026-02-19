b394f064593b04b19e67a8366954b294d3cba51e
Eredin <britcovegor@gmail.com>
2026-02-19 04:02:29 +0200

    first programm in PHP

---
+++ Lab02/index.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Hello, World with echo!";
+echo "<br />";
+
+print "Hello, World with print!";
+echo "<br /><br />";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+// Конкатенация
+echo "Количество дней: " . $days . "<br />";
+echo $message . "<br /><br />";
+
+// двойные кавычки
+echo "Количество дней: $days<br />";
+echo "$message";
+
+?>
+
+
