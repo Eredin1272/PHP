736b4b0a507b506ab609f7971645ebe61f01a4ac
Eredin <britcovegor@gmail.com>
2026-04-26 19:49:08 +0300

    aeada

--- lab07/project/index.php
+++ lab07/project/index.php
@@ -2,6 +2,14 @@
 
 require_once 'src/functions.php';
 require_once 'src/handler.php';
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates_twig');
+$twig = new \Twig\Environment($loader);
+
+$twig->addFilter(new \Twig\TwigFilter('stars', function ($rating) {
+    return str_repeat('⭐', $rating);
+}));
 
 $page = $_GET['page'] ?? 'form';
 $sort = $_GET['sort'] ?? '';
@@ -29,3 +37,14 @@ if ($page === 'list') {
 $content = ob_get_clean();
 
 include 'templates/layout.php';
+
+if ($page === 'list') {
+    echo $twig->render('list.twig', [
+        'games' => $games
+    ]);
+} else {
+    echo $twig->render('form.twig', [
+        'errors' => $errors
+    ]);
+}
+
